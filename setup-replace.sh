6378e5387c7947b06e1f42c8a91d5e0de8b844ae
Mahesh <59241149+hoistedNinja@users.noreply.github.com>
2021-06-29 19:05:29 -0500

    Update setup-replace.sh
    Included manual steps on yaml to this setup script

--- setup-replace.sh
+++ setup-replace.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
+# installing pipenv and dependencies
+python --version
+ls
+cd code
+pip install --user pipenv
+pipenv lock -r > requirements.txt
+if [ -f requirements.txt ]; then pipenv install -r requirements.txt; fi
+cd ..
 
+#making settings folder
 mkdir -p settings
 cp -vr examples/settings/* settings/
 
@@ -9,3 +18,12 @@ mkdir -p logs/queue logs/generated logs/temp
 # Disable JSON logs from engine; these are placed elsewhere
 cd logs/temp
 test -e log.json || ln -s /dev/null log.json
+ls
+echo "Done with creation of settings"
+# checking if cast.py is functional with installed dependencies and pipenv
+cd code
+echo "Printing pipenv graph"
+pipenv graph
+echo "Running cast.py from pipenv shell"
+pipenv run python3 cast.py
+echo "set up complete sucessfully"
